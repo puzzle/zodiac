e93736b87b91b21ecfe8589a3c38b17d337702ab
Thomas Burkhalter <burkhalter@puzzle.ch>
2019-07-01 14:51:06 +0200

    Fix dates, source: WIKIPEDIA

--- lib/zodiac/finder.rb
+++ lib/zodiac/finder.rb
@@ -26,45 +26,43 @@ module Zodiac
       range_for(4,  21, 5,  21) => :taurus,
       range_for(5,  22, 6,  21) => :gemini,
       range_for(6,  22, 7,  22) => :cancer,
-      range_for(7,  23, 8,  21) => :leo,
-      range_for(8,  22, 9,  23) => :virgo,
+      range_for(7,  23, 8,  22) => :leo,
+      range_for(8,  23, 9,  23) => :virgo,
       range_for(9,  24, 10, 23) => :libra,
       range_for(10, 24, 11, 22) => :scorpio,
-      range_for(11, 23, 12, 22) => :sagittarius,
-      range_for(12, 23, 12, 31) => :capricorn
-    }
-    
+      range_for(11, 23, 12, 21) => :sagittarius,
+      range_for(12, 22, 12, 31) => :capricorn
+    }.freeze
+
     SIGN_IDS = {
-      aries:       1,
-      taurus:      2,
-      gemini:      3,
-      cancer:      4,
-      leo:         5,
-      virgo:       6,
-      libra:       7,
-      scorpio:     8,
+      aries: 1,
+      taurus: 2,
+      gemini: 3,
+      cancer: 4,
+      leo: 5,
+      virgo: 6,
+      libra: 7,
+      scorpio: 8,
       sagittarius: 9,
-      capricorn:   10,
-      aquarius:    11,
-      pisces:      12
-    }
-    
+      capricorn: 10,
+      aquarius: 11,
+      pisces: 12
+    }.freeze
+
     class << self
       def sign_for(date)
-        I18n.t!("zodiac.#{self.sign_symbol_for date}")
+        I18n.t!("zodiac.#{sign_symbol_for date}")
       end
-      
+
       def sign_id_for(date)
-        SIGN_IDS[self.sign_symbol_for date]
+        SIGN_IDS[sign_symbol_for date]
       end
-      
+
       def sign_symbol_for(date)
         RANGES.each do |range, sign|
-          if range.days.cover? date_for(date[:month], date[:day])
-            return sign
-          end
+          return sign if range.days.cover? date_for(date[:month], date[:day])
         end
-        
+
         raise ArgumentError
       end
     end
